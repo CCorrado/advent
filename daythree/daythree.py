63fe01414e9e7c940988a3278c5e7f766afb4408
ccorrado <ccorrad@its.jnj.com>
2018-12-17 14:32:11 -0500

    you wot

--- daythree/daythree.py
+++ daythree/daythree.py
@@ -19,45 +19,34 @@ def get_result(file_path):
                 xy = (int(coord[coord.index(",") + 1:]) + 1, int(coord[:coord.index(",")]) + 1)
                 coord_set_set = (xy, wh)
                 fd[line[line.index("#") + 1:line.index("@") - 1]] = coord_set_set
-            hits: defaultdict(tuple) = calculate_coverage(fd)
-            return len(hits)
+            hits: int = calculate_coverage(fd)
+            return hits
 
 
-def calculate_coverage(fd: dict) -> defaultdict(tuple):
+def calculate_coverage(fd: dict) -> int:
     """
     Method to calculate the coverage of a single entry.
 
     :param fd: dictionary containing a reference to the ID with the associated coord and size.
     :return: tuple containing the (Coordinate, List<Int> of covered coordinates).
     """
-    hits = defaultdict(tuple)
-    dups = defaultdict(tuple)
-    x_hits = defaultdict(list)
-    y_hits = defaultdict(list)
-    x_coords = set()
-    y_coords = set()
+    hits = defaultdict(list)
+    dups = defaultdict(list)
+    miss = defaultdict(list)
     for key, value in fd.items():
-        # x values first
-        for x_coord in range(value[0][0], value[0][0] + value[1][0]):
-            x_hits[x_coord] += [key]
-            x_coords.add(x_coord)
-        for y_coord in range(value[0][1], value[0][1] + value[1][1]):
-            y_hits[y_coord] += [key]
-            y_coords.add(y_coord)
+        for x in range(value[0][0], value[0][0] + value[1][0]):
+            for y in range(value[0][1], value[0][1] + value[1][1]):
+                if (x, y) in hits:
+                    dups[(x, y)] += hits[(x, y)] + [key]
 
-    max_x = max(x_coords)
-    max_y = max(y_coords)
+                if key in dups.values():
+                    miss[key] = []
+                else:
+                    miss[key] += [key]
 
-    for x in range(0, max_x):
-        for y in range(0, max_y):
-            if x_hits[x] != [] and y_hits[y] != []:
-                for x_hit, y_hit in zip(x_hits[x], y_hits[y]):
-                    if x_hit == y_hit:
-                        hits[(x, y)] += tuple(x_hit)
-    for key, hit in hits.items():
-        if len(hit) > 1:
-            dups[key] += hit
-    return dups
+                hits[(x, y)] += [key]
+
+    return len(dups)
 
 
 if __name__ == "__main__":
--- daythree/daythreetests.py
+++ daythree/daythreetests.py
@@ -7,3 +7,6 @@ class DayThreeTests(unittest.TestCase):
 
     def test_one(self):
         self.assertEqual(4, get_result("testdata1"))
+
+    def test_two(self):
+        self.assertEqual(103806, get_result("daythreeinput.txt"))
